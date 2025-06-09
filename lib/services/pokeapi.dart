41c40e91314ec6eddda0b3012b50b92711210996
kruegerm97 <kruegermaurice97@gmail.com>
2025-06-09 17:01:06 +0200

    pokeapi dummy functions

--- lib/services/pokeapi.dart
+++ lib/services/pokeapi.dart
@@ -1,3 +1,26 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+class PokeApiService {
+  final String baseUrl = 'https://pokeapi.co/api/v2';
+
+  Future<Map<String, dynamic>> fetchPokemon(String name) async {
+    final response = await http.get(Uri.parse('$baseUrl/pokemon/$name'));
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load Pokemon');
+    }
+  }
+
+  Future<List<dynamic>> fetchPokemonList(int limit, int offset) async {
+    final response = await http.get(Uri.parse('$baseUrl/pokemon?limit=$limit&offset=$offset'));
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body)['results'];
+    } else {
+      throw Exception('Failed to load Pokemon list');
+    }
+  }
+}
